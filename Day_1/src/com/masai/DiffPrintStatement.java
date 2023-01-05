7cee96d076a9c97611b5c282d55be984cca34499
Shubham Bhati <112773220+Shubh2-0@users.noreply.github.com>
2023-01-06 01:08:42 +0530

    Update DiffPrintStatement.java

--- Day_1/src/com/masai/DiffPrintStatement.java
+++ Day_1/src/com/masai/DiffPrintStatement.java
@@ -10,7 +10,7 @@ public class DiffPrintStatement {
 		System.out.print("How I wonder what you are\n");
 		System.out.println("Up above the world so high");
 		System.out.println("Like a diamond in the sky");
-     		
+//      		
 		
 	}
 
