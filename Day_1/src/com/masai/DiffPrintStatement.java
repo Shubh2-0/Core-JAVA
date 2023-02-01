abf919839878e18938b612b74db90c4e8f5ed825
Shubham Bhati <112773220+Shubh2-0@users.noreply.github.com>
2023-02-02 00:47:03 +0530

    Revert "Update Code"

--- Day_1/src/com/masai/DiffPrintStatement.java
+++ Day_1/src/com/masai/DiffPrintStatement.java
@@ -10,7 +10,6 @@ public class DiffPrintStatement {
 		System.out.print("How I wonder what you are\n");
 		System.out.println("Up above the world so high");
 		System.out.println("Like a diamond in the sky");
-		System.out.println("Hello World - Testing");
 //      		
 // 		
 	}
